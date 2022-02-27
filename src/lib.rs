f8cfa04c4154d5bc6f6f35d9fa2645a6cb90bb3c
Nathan Marley <nathan.marley@gmail.com>
2022-02-27 09:18:41 -0300

    WIP and stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use walkdir::WalkDir;
 
 #[derive(Parser, Debug)]
 #[clap(version = "0.1")]
@@ -32,6 +33,7 @@ struct Args {
     pub filter: Option<u32>,
 }
 
+// also: consider an internal webserver which serves up the UI for blu
 pub fn run() -> Result<(), Box<dyn std::error::Error>> {
     let mut args: Args = Args::parse();
 
@@ -40,5 +42,45 @@ pub fn run() -> Result<(), Box<dyn std::error::Error>> {
     }
     println!("args: {:?}", args);
 
+    // let key = read-key-from-.blu/metadata.json;
+    // decrypt somehow?
+
+    // let conn = db.connection();
+    let dir = ".";
+    index(dir)
+    // sync()
+}
+
+// walk the dir and hash all regular files
+// ignore block/char specials
+//
+// TODO: need to accept an SQLite3 connection for metadata writes
+fn index(base_dir: &str) -> Result<(), Box<dyn std::error::Error>> {
+    for entry in WalkDir::new(base_dir).into_iter().filter_map(|e| e.ok()) {
+        // TODO: allow symlinks?
+        // if !entry.file_type().is_file() {
+        //     continue;
+        // }
+        // count += 1;
+
+        // let metadata = fs::metadata(entry.path())?;
+        // let size = metadata.len();
+        // // println!("{:?}: {:?} bytes", entry.path(), size);
+
+        // if size > max {
+        //     max = size;
+        //     biggest = entry.path().to_path_buf();
+        // }
+        // if size < min {
+        //     min = size;
+        //     smallest = entry.path().to_path_buf();
+        // }
+    }
+
     Ok(())
 }
+
+// fn sync() -> Result<(), Box<dyn std::error::Error>> {
+//     Err("something didn't work".into())
+//     // Ok(())
+// }
