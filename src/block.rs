44b628dbdb6b60b993239860af17d2a03451b1bb
Nathan Marley <nathan.marley@gmail.com>
2022-04-24 21:37:54 -0300

    Fix Chunkerator and test

--- src/block.rs
+++ src/block.rs
@@ -40,15 +40,15 @@ impl PlainFileIndex {
                 continue;
             }
 
-            let chunk_oh = Chunkerator::new(&elem.path());
             let mut chunkmetas: Vec<ChunkMeta> = vec![];
             let mut hasher = Sha2_512::default();
-            for chunk in chunk_oh {
-                dbg!(&hex::encode(&chunk));
+            let chunker = Chunkerator::new(&elem.path(), BLOCK_SIZE)?;
+            for chunk in chunker {
                 chunkmetas.push(ChunkMeta::new(&chunk));
                 hasher.update(&chunk);
             }
-            let file_hash = Hash::from(Code::Sha2_512.wrap(&hasher.finalize()).unwrap().to_bytes());
+            let file_mh = Code::Sha2_512.wrap(hasher.finalize())?;
+            let file_hash = Hash::from(file_mh.to_bytes());
 
             let fileref = map
                 .entry(file_hash)
@@ -228,94 +228,55 @@ impl ChunkMeta {
     }
 }
 
-// (Hash, ChunkMetas)
-// pub fn hash_and_chunk<P: AsRef<Path>>(
-//     filepath: P,
-// ) -> Result<Vec<Self>, Box<dyn std::error::Error>> {
-//         let f = std::fs::File::open(filepath).unwrap();
-//         let mut reader = BufReader::with_capacity(BLOCK_SIZE, f);
-//         let mut chunkmetas: Vec<Self> = vec![];
-//         let mut count: usize = 0;
-//         while let Ok(data) = reader.fill_buf() {
-//             if data.is_empty() {
-//                 break;
-//             }
-//             let actual_data = data.to_vec();
-//             chunkmetas.push(Self::new(&actual_data));
-//             reader.consume(actual_data.len());
-//             count += 1;
-//         }
-//         Ok(chunkmetas)
-//     }
-//
-// let file_data = std::fs::read(&elem.path())?;
-// let chunkmetas = ChunkMeta::read_from_disk(&elem.path())?;
-// let file_hash = Hash::from(hash::multihash(&file_data).to_bytes());
-
-#[derive(Default, Debug)]
+/// Chunkerator reads files a "chunk" at a time, and returns chunks via the
+/// iterator.
+#[derive(Debug)]
 pub struct Chunkerator {
     filepath: PathBuf,
-    offset: u64,
-    // buf: Vec<u8>,
+    buf_reader: BufReader<std::fs::File>,
 }
 
-// NGM
 impl Chunkerator {
-    // TODO: Pass block size here, don't hard-code
-    fn new<P: AsRef<Path>>(filepath: P) -> Self {
-        Self {
+    fn new<P: AsRef<Path>>(
+        filepath: P,
+        chunk_size: usize,
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        let f = std::fs::File::open(filepath.as_ref()).unwrap();
+        let reader = BufReader::with_capacity(chunk_size, f);
+        Ok(Self {
             filepath: filepath.as_ref().to_path_buf(),
-            offset: 0,
-            // buf: vec![0; BLOCK_SIZE],
-        }
+            buf_reader: reader,
+        })
     }
 }
 
 impl std::iter::Iterator for Chunkerator {
     type Item = Vec<u8>;
     fn next(&mut self) -> Option<Self::Item> {
-        let mut f = match std::fs::File::open(&self.filepath) {
+        // fill entire reader
+        let data = match self.buf_reader.fill_buf() {
+            Ok(data) => data,
             Err(e) => {
-                error!("reading file in Chunklifier: {}", e);
+                error!("Chunkerator read error: {}", e);
                 return None;
             }
-            Ok(f) => f,
         };
-
-        // let mut reader = BufReader::with_capacity(BLOCK_SIZE, &f);
-        let mut buf: Vec<u8> = vec![0; BLOCK_SIZE];
-
-        let _seek_offset = match f.seek(SeekFrom::Start(self.offset)) {
-            Err(e) => {
-                error!("error in seek in Chunklifier: {}", e);
-                return None;
-            }
-            Ok(offset) => offset,
-        };
-        if let Err(e) = f.read_exact(&mut buf) {
-            error!("error reading bytes in Chunklifier: {}", e);
+        // handle None case (no more data to read)
+        if data.is_empty() {
             return None;
-        };
-        self.offset += buf.len() as u64;
-        info!("offset (AFTER UPDATING in Chunklifier): {}", self.offset);
-
-        Some(buf)
-
-        // TODO: None case here ...
-        // while let Ok(data) = reader.fill_buf()
-        //     if data.is_empty() {
-        //         break;
-        //     }
-        //     let actual_data = data.to_vec();
-        //     chunkmetas.push(Self::new(&actual_data));
-        //     reader.consume(actual_data.len());
-        // Ok(chunkmetas)
+        }
+        let data = data.to_vec();
+        self.buf_reader.consume(data.len());
+        Some(data)
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::{BlockRef, ChunkMeta, Chunkerator, FileRef, Hash, PlainBlockIndex, PlainFileIndex};
+    use super::{
+        BlockRef, ChunkMeta, Chunkerator, FileRef, Hash, PlainBlockIndex, PlainFileIndex,
+        BLOCK_SIZE,
+    };
     use std::collections::{HashMap, HashSet};
     use std::path::Path;
 
@@ -495,20 +456,10 @@ mod test {
 
     #[test]
     fn chunkerator() {
-        // let file1_path = Path::new(TEST_BLOCKS_DIR_T1).join("file1.txt");
         let file5_path = Path::new(TEST_BLOCKS_DIR_T1).join("file5.txt");
-
-        // TODO: Pass block size here, don't hard-code
-        let chunker = Chunkerator::new(file5_path);
-        let mut iter = chunker.into_iter();
-        assert!(!iter.next().is_none());
-        // for chunk in chunker {
-        //     dbg!(&chunk);
-        //     assert!(chunk.len() > 99999);
-        // }
-        // let file_hash = Hash::from(Code::Sha2_512.wrap(&hasher.finalize()).unwrap().to_bytes());
-
-        // assert_eq!(block_index.count_blocks(), 5);
-        // assert_eq!(block_index, PlainBlockIndex { map });
+        let mut chunker = Chunkerator::new(file5_path, BLOCK_SIZE).unwrap();
+        let chunk = chunker.next();
+        assert!(chunk.is_some());
+        assert_eq!(chunk.unwrap().len(), 1024);
     }
 }
