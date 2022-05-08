01f02d762e4c90647e1f6780ed230c194f4129d3
Nathan Marley <nathan.marley@gmail.com>
2022-05-07 22:52:45 -0300

    Tidy encrypt_files util

--- src/bin/encrypt_files.rs
+++ src/bin/encrypt_files.rs
@@ -31,63 +31,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     dbg!(&cfg);
 
     let index = PlainIndex::new(dir)?;
-    dbg!(&index);
+    // dbg!(&index);
 
-    let mut cfm = BlobManager::new(&cfg.datadir(), &bbox);
-    dbg!(&cfm);
+    let mut blob_mgr = BlobManager::new(&cfg.datadir(), &bbox);
+    // dbg!(&blob_mgr);
 
-    // How to know if encryption is needed or not? It's possible the chunk is
-    // already encrypted ...
-    for (block_hash, block_ref) in index.blocks_map_ref().iter() {
-        dbg!(&block_ref);
-        if let Some(enc_hash) = &block_ref.encrypted_hash {
-            println!(
-                "block hash {:?} already encrypted with enc hash {:?} ...",
-                block_hash, enc_hash
-            );
-        } else {
-            // encrypt
-            println!("encrypt this chunk!!");
-            let mut chunk_bytes = index.get_chunk_bytes(block_ref);
-            cfm.add_chunk(&mut chunk_bytes)?;
-        }
+    // Let the BlobManager determine if we need to encrypt something ...
+    for (_block_hash, block_ref) in index.blocks_map_ref().iter() {
+        // dbg!(&block_ref);
+        let mut chunk_bytes = index.get_chunk_bytes(block_ref);
+        blob_mgr.add_chunk(&mut chunk_bytes)?;
     }
-
-    // for (_file_hash, fileref) in index.files_map_ref().iter() {
-    //     // dbg!(&file_hash);
-    //     dbg!(&fileref);
-    //     // iterate over plain chunks in file ...
-    //     let fri = fileref.iter()?;
-    //     // for (count_chunk, plain_data_chunk) in fri.enumerate() {
-    //     //     // 1. encrypt plain data chunk
-    //     //     // 2. use cfm to add ...
-    //     //     // 3. ... finalize cfm when done?
-    //     //     dbg!(&hex::encode(&plain_data_chunk));
-    //     //     let enc_chunk = bbox.encrypt(&plain_data_chunk)?;
-    //     //     match cfm.add_chunk(&enc_chunk)? {
-    //     //         CFAddStatus::WrittenToDisk(path) => {
-    //     //             // update path here ...
-    //     //             // index.update_encrypted(plain_chunk_hash, encrypted_hash);
-    //     //             println!("Wrote chunkfile to disk at {}", path.display());
-    //     //         }
-    //     //         CFAddStatus::AddedToMemory => {
-    //     //             // do nothing ...
-    //     //             println!("Added to memory in active chunkfile");
-    //     //         }
-    //     //         CFAddStatus::NothingToDo => {
-    //     //             // do nothing ...
-    //     //             println!("Nothing to do");
-    //     //         }
-    //     //     };
-    //     //     println!(
-    //     //         "count_chunk = {} -------------------------------------------------------",
-    //     //         count_chunk
-    //     //     );
-    //     // }
-    //     println!("========================================================================");
-    // }
-    // TODO: update path in indexes
-    // let path = cfm.finalize();
 
     Ok(())
 }
