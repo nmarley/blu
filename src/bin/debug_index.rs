026ef200d77413456afdb8a6e689bfa54f883ff2
Nathan Marley <nathan.marley@gmail.com>
2022-12-29 22:04:19 -0300

    Show size of index

--- src/bin/debug_index.rs
+++ src/bin/debug_index.rs
@@ -13,6 +13,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut index = PlainIndex::new(index_dir)?;
     dbg!(&index);
+    println!("num bytes indexed: {}", index.num_bytes_indexed());
 
     let old_filename = Path::new(index_dir).join("hi.txt");
     let new_filename = Path::new(index_dir).join("hello.txt");
@@ -21,6 +22,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let tuples = index.update(index_dir)?;
     dbg!(&index);
+    println!("num bytes indexed: {}", index.num_bytes_indexed());
+
     dbg!(&tuples);
     // move it back
     std::fs::rename(&new_filename, &old_filename)?;
