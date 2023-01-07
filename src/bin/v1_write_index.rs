b0ae5cba632f01f278ee6dcb98087f8fefa13a50
Nathan Marley <nathan.marley@gmail.com>
2023-01-06 22:54:20 -0300

    Update v1_write_index util (more flexible)

--- src/bin/v1_write_index.rs
+++ src/bin/v1_write_index.rs
@@ -1,9 +1,17 @@
+use clap::Parser;
+use std::fs;
 use std::io::Write;
-use std::{env, fs};
+use std::path::Path;
 
 use blu::age::BlackBox;
 use blu::config;
 use blu::metadata::{EncryptedIndex, Index};
+
+#[derive(Parser)]
+pub struct Args {
+    pub dir: String,
+    pub outfile: String,
+}
 
 const TEST_AGE_SECRET_KEY: &str = include_str!("../../test/blu_secrets/blu.key");
 const V1_WARNING: &str = "WARNING: This tool is from v0.1 and will not work w/current codebase";
@@ -11,17 +19,13 @@ const V1_WARNING: &str = "WARNING: This tool is from v0.1 and will not work w/cu
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("\n{}\n", V1_WARNING);
 
-    let mut args = env::args();
-    if args.len() == 1 {
-        eprintln!("usage: {} <dir-to-index>", args.next().unwrap());
-        std::process::exit(1);
-    }
-    let dir = &args.nth(1).unwrap();
+    let args = Args::parse();
+    let dir = args.dir;
 
     let bbox = BlackBox::new(&[TEST_AGE_SECRET_KEY]);
-    let mut index = Index::new(dir)?;
+    let mut index = Index::new(&dir)?;
 
-    let cfg = config::read_config(dir)?;
+    let cfg = config::read_config(&dir)?;
     dbg!(&cfg);
 
     let enc_idx = EncryptedIndex::new(cfg.datadir())?;
@@ -32,16 +36,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // dbg!(&index);
 
+    let outfile = args.outfile;
     // writing index for testing
-    write_index_file(&index, &bbox)?;
+    write_index_file(&index, &bbox, &outfile)?;
 
     Ok(())
 }
 
-fn write_index_file(index: &Index, bbox: &BlackBox) -> Result<(), Box<dyn std::error::Error>> {
+fn write_index_file<P: AsRef<Path>>(
+    index: &Index,
+    bbox: &BlackBox,
+    outfile: P,
+) -> Result<(), Box<dyn std::error::Error>> {
     let mut enc_idx_bytes = Vec::new();
     index.write(&mut enc_idx_bytes, bbox)?;
-    let mut file = fs::File::create("test-idx-enc.dat")?;
+    let mut file = fs::File::create(outfile)?;
     file.write_all(&enc_idx_bytes)?;
     Ok(())
 }
