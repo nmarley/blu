9b0f7786f4779485ef3a713be86e7ac415630d3f
Nathan Marley <nathan.marley@gmail.com>
2023-01-08 12:46:48 -0300

    Adjust encrypt_files util for new refactor

--- src/bin/encrypt_files.rs
+++ src/bin/encrypt_files.rs
@@ -1,9 +1,9 @@
 use std::env;
 
 use blu::age::BlackBox;
-use blu::block::{PlainIndex, INDEX_FILENAME};
+use blu::blob::BlobBuffer;
 use blu::config;
-use blu::io::BlackBoxSerializable;
+use blu::hash::{self, Hash};
 
 const TEST_AGE_SECRET_KEY: &str = include_str!("../../test/blu_secrets/blu.key");
 
@@ -22,26 +22,59 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         eprintln!("More info: {}", e);
         e
     })?;
-    dbg!(&cfg);
+    // dbg!(&cfg);
 
-    let datadir = cfg.datadir();
-    dbg!(&datadir);
+    let _datadir = cfg.datadir();
+    // dbg!(&datadir);
 
-    let index = cfg.load_plain_index(&bbox).unwrap();
-    // dbg!(&index);
+    let plain_index = cfg.load_plain_index(&bbox).unwrap();
+    // dbg!(&plain_index);
 
     // TODO: ... do we only encrypt the files in index, or do we add/update
     // files, THEN encrypt everything that is not already encrypted?
 
-    //let mut blob_mgr = BlobManager::new(&cfg.datadir(), Some(bbox));
-    //// dbg!(&blob_mgr);
-    //
-    // Let the BlobManager determine if we need to encrypt something ...
-    //for (_block_hash, block_ref) in index.blocks_map_ref().iter() {
-    //    // dbg!(&block_ref);
-    //    let mut chunk_bytes = index.get_chunk_bytes(block_ref);
-    //    blob_mgr.add_chunk(&mut chunk_bytes)?;
-    //}
+    let mut blob_index = cfg.load_blob_index(&bbox).unwrap_or_default();
+    let mut blob_buf = BlobBuffer::new(cfg.datadir(), bbox.clone());
+    dbg!(&blob_buf);
+
+    // TODO: determine whether to use underscore in e.g. block_hash or
+    // blockhash, block_ref or blockref, and then stay consistent thru the
+    // codebase.
+
+    // need some kind of selection mechanism here -- which files to encrypt?
+    // for now, we encrypt them all and sort the selection out later
+    for (blockhash, blockref) in plain_index.blocks_map_ref() {
+        // let block = index.get_block(blockref).unwrap();
+        // dbg!(&blockhash);
+        if blob_index.has_chunk(blockhash) {
+            println!("already encrypted: {:?}", blockhash);
+            continue;
+        }
+
+        println!("NOT encrypted: {:?}, adding ...", blockhash);
+        let data = plain_index.read_block_bytes(blockref);
+        // println!("data: {:?}", data);
+
+        // NOTE: we probably want to somehow keep this around / add it as a
+        // checksum to ensure that the data is not corrupted
+        let block_hash2 = Hash::from(hash::multihash(&data).to_bytes());
+        assert_eq!(
+            blockhash, &block_hash2,
+            "blockhash mismatch (unresolvable black death of the universe error)"
+        );
+
+        // add it to the blob buffer
+        blob_buf.add_chunk(&mut data.clone(), &mut blob_index)?;
+        println!("========================================================================");
+    }
+    match blob_buf.finalize(&mut blob_index) {
+        Ok(_) => println!("Finalized blob buffer!"),
+        Err(e) => println!("Error finalizing blob buffer: {}", e),
+    }
+    match cfg.write_blob_index(&blob_index, &bbox) {
+        Ok(_) => println!("Wrote blob index!"),
+        Err(e) => println!("Error writing blob index: {}", e),
+    }
 
     Ok(())
 }
