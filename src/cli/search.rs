2cb7958c7f751793998d02c135dab5d66d5ad91c
Nathan Marley <nathan.marley@gmail.com>
2024-01-15 10:50:50 -0600

    Tidy search util a bit

--- src/cli/search.rs
+++ src/cli/search.rs
@@ -9,12 +9,9 @@ use crate::search::FilenameSearchIndex;
 
 const TEST_AGE_SECRET_KEY: &str = include_str!("../../test/blu_secrets/blu.key");
 
-/// Search files was I believe supposed to be a frontend for the
-/// full-text-search index for filenames.
-///
-/// Note: this is likely not finished or stable.
+/// Search for filenames or tags
 pub fn search(args: SearchArgs) -> Result<(), Box<dyn std::error::Error>> {
-    info!("Started search util");
+    // info!("Started search util");
 
     let dir = Path::new(".");
 
@@ -28,14 +25,21 @@ pub fn search(args: SearchArgs) -> Result<(), Box<dyn std::error::Error>> {
     // TODO: load search index here ... (once implemented)
     //   for now, just create a new one every time and then search
     let index = cfg.load_plain_index(&bbox).unwrap();
-    let mut search_index = FilenameSearchIndex::new();
-
+    let mut filename_search_index = FilenameSearchIndex::new();
     let files_map = index.files_map_ref();
     for (file_hash, file_ref) in files_map {
         // info!("file_hash: {}", file_hash.dbg_short(7));
         for path in file_ref.paths.iter() {
-            search_index.add_filename(path.to_str().unwrap(), file_hash);
+            filename_search_index.add_filename(path.to_str().unwrap(), file_hash);
         }
+    }
+
+    let search_result = filename_search_index.search(&args.needle);
+    info!("Got {} result(s)", search_result.len());
+    for file_hash in search_result.iter() {
+        info!("{:?}", file_hash);
+        // let file_ref = files_map.get(file_hash).unwrap();
+        // info!("{:?}", file_ref);
     }
 
     // load tag index
@@ -44,13 +48,8 @@ pub fn search(args: SearchArgs) -> Result<(), Box<dyn std::error::Error>> {
         dbg!(&tag_search_result);
     };
 
-    let search_result = search_index.search(&args.needle);
-    info!("Got {} result(s)", search_result.len());
-
-    for file_hash in search_result.iter() {
-        let file_ref = files_map.get(file_hash).unwrap();
-        info!("{:?}", file_ref);
-    }
+    // TODO: unify the results above and use a FileDisplay to display the
+    // output, kinda status does
 
     Ok(())
 }
