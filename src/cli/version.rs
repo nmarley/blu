9a5837880bf42015a8c139e169a30d70c809a2c3
Nathan Marley <nathan.marley@gmail.com>
2024-04-21 09:22:24 -0500

    version: Clippy

--- src/cli/version.rs
+++ src/cli/version.rs
@@ -5,8 +5,8 @@ pub async fn version(_args: EmptyArgs) -> Result<(), Box<dyn std::error::Error>>
     // git version 2.44.0
     println!(
         "{} version {}",
-        env!("CARGO_PKG_NAME").to_string(),
-        env!("CARGO_PKG_VERSION").to_string(),
+        env!("CARGO_PKG_NAME"),
+        env!("CARGO_PKG_VERSION"),
     );
     Ok(())
 }
